fb3f331895c25c2689c28ebf73191b50459f525f
Женя Кушнир <kushnir.ea@phystech.edu>
2021-12-16 23:29:41 +0300

    WTF fixed serialization? we believe in git!

--- data.py
+++ data.py
@@ -1,19 +1,32 @@
 """
 Модуль для сериализации и десериализации игрового прогресса между входами и выходами из программы. 
 """
+import pickle
+
 import model
+import json
+
+
 
 PATH = ""  # FIXME
 
 
 def deserialise() -> model.World:
-    """Выгружает из системного файла состояние мира и инициализирует его.
+    """Выгружает из системного файла world.saviour состояние мира и инициализирует его.
     returns: world в состоянии на момент последнего выхода из игры."""
-    # FIXME
-    return None
+
+    with open('world.saviour', 'rb') as f:
+        world = pickle.load(f)
+
+    # проверить работоспособность
+    return world
 
 
 def serialise(world):
-    """Сохраняет состояние мира в системный файл.""" 
-    # FIXME
-    pass
+    """Сохраняет состояние мира в системный файл."""
+
+    with open('world.saviour', 'wb') as f:
+        pickle.dump(world, f)
+
+
+    # проверить работоспособностьgit
