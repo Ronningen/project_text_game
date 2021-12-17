34fec5795385447748b8abdbf5581160df047381
Женя Кушнир <kushnir.ea@phystech.edu>
2021-12-17 06:51:10 +0300

    serialization with json

--- data.py
+++ data.py
@@ -4,17 +4,19 @@
 
 import pickle
 import model
+import json
+
 def deserialise() -> model.World:
     """Выгружает из системного файла world.saviour состояние мира и инициализирует его.
     returns: world в состоянии на момент последнего выхода из игры."""
-    return None
-    with open('world.saviour', 'rb') as f:
-        world = pickle.load(f)
+    with open('world.saviour', 'r') as f:
+        world = json.load(f)
         return world
 
 
 def serialise(world):
     """Сохраняет состояние мира в системный файл."""
 
-    with open('world.saviour', 'wb') as f:
-        pickle.dump(world, f)
+    with open('world.saviour', 'w') as f:
+        json.dump(world, f)
+
