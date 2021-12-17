c0837f8795eaac39a8fb1e1ddcc861e0ca4d5bf5
Женя Кушнир <kushnir.ea@phystech.edu>
2021-12-17 07:42:43 +0300

    Комит им. Александра

--- data.py
+++ data.py
@@ -2,21 +2,23 @@
 Модуль для сериализации и десериализации игрового прогресса между входами и выходами из программы. 
 """
 
+import os
 import pickle
 import model
-import json
-
 def deserialise() -> model.World:
     """Выгружает из системного файла world.saviour состояние мира и инициализирует его.
     returns: world в состоянии на момент последнего выхода из игры."""
-    with open('world.saviour', 'r') as f:
-        world = json.load(f)
-        return world
+#    return None
+    if os.path.getsize('world.saviour') > 0:
+        with open('world.saviour', 'rb') as f:
+            world = pickle.load(f)
+            return world
+    else:
+        return None
 
 
 def serialise(world):
     """Сохраняет состояние мира в системный файл."""
 
-    with open('world.saviour', 'w') as f:
-        json.dump(world, f)
-
+    with open('world.saviour', 'wb') as f:
+        pickle.dump(world, f, protocol = 0)
